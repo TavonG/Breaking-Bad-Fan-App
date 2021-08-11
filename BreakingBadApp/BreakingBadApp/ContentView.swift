f35d631a3be45c81a61fc7336b6e6e00bb82124e
TavonG <iostavong@gmail.com>
2021-08-10 22:39:50 -0500

    the start

--- BreakingBadApp/BreakingBadApp/ContentView.swift
+++ BreakingBadApp/BreakingBadApp/ContentView.swift
@@ -7,15 +7,29 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            TabView {
+                MainView()
+                    .tabItem {
+                        Label("Characters", systemImage: "person.3")
+                    }
+                EpisodesView()
+                    .tabItem {
+                        Label("Episodes", systemImage: "list.number")
+                    }
+                
+            }
+            .navigationTitle("Breaking Bad Fans")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        
     }
 }

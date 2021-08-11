a9cce295dc020a70a01f750cfa2fa03e804600f4
TavonG <iostavong@gmail.com>
2021-08-10 20:32:27 -0500

    First Command

---
+++ BreakingBadFanApp/BreakingBadFanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadFanAppApp.swift
+//  BreakingBadFanApp
+//
+//  Created by Tavon Gibbs on 8/10/21.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadFanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
